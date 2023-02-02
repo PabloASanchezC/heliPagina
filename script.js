7a28c6adda12fc863111b25d471df303f0403d08
Pablo <pspablosanchezc@gmail.com>
2023-02-02 16:01:45 -0600

    carrousel

--- script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burgerMenu");
-const navMenu = document.querySelector(".indice");
-burger.addEventListener("click", toggleNavMenu);
-
-function toggleNavMenu() {
-  navMenu.classList.toggle("inactive");
-}
